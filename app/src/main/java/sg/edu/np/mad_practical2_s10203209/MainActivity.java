f2f00b545ecf312502d38332b97c50ecda953235
Le Xuan Tricia Tan <s10203209@connect.np.edu.sg>
2021-05-01 00:55:16 +0800

    Test

--- app/src/main/java/sg/edu/np/mad_practical2_s10203209/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad_practical2_s10203209/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private final static String TAG = "Main Activity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
